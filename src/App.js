77df0398ef500f4aed6933ef7602059bfeda32cd
Vincent <makokhavomondi@gmail.com>
2023-01-23 22:42:46 +0300

    Style the items

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         {
           [1, 2, 3].map(index => {
             return (
-              <div key={`item-&-btn-${index}`} className='item-and-btn'>
+              <div key={`item-and-btn-${index}`} className={activeCanvas == index? 'active item-and-btn': 'item-and-btn'}>
                   <CanvasItem  index={index}/>
                   <CanvasItemBtn index={index} activeCanvas={activeCanvas} setActiveCanvas={setActiveCanvas}/>
               </div>
@@ -25,7 +25,9 @@ function App() {
         {
           [4, 5, 6].map(index => {
             return (
-                <CanvasItem key={`canvas-${index}`} index={index}/>
+              <div key={`item-and-btn-${index}`} className='item-and-btn'>
+                <CanvasItem index={index}/>
+              </div>
             )
           })
         }
