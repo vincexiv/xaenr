3cac6ee69974505577f07150803053c5edef5742
Vincent <makokhavomondi@gmail.com>
2023-01-24 12:36:31 +0300

    Replace == with ===

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           {
             [1, 2, 3].map(index => {
               return (
-                <div key={`item-and-btn-${index}`} className={activeCanvas == index? 'active item-and-btn': 'item-and-btn'}>
+                <div key={`item-and-btn-${index}`} className={activeCanvas === index? 'active item-and-btn': 'item-and-btn'}>
                     <CanvasItem  index={index}/>
                     <CanvasItemBtn index={index} activeCanvas={activeCanvas} setActiveCanvas={setActiveCanvas}/>
                 </div>
