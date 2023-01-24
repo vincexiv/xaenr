580f79b3997273db971794d569bf21ae5f34601a
Vincent <makokhavomondi@gmail.com>
2023-01-24 17:49:50 +0300

    Make navbar buttons darker and enable switching active one

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.css'
 
 function Navbar(){
+    const [activeAction, setActiveAction] = useState('how-it-works')
+
     return (
         <nav>
             <div className='nav-content'>
                 <h1 className='company-name'>xaenr</h1>
                 <div className='actions'>
-                    <p id='how-it-works' className='item'>How It Works</p>
-                    <p id='how-to-test-it' className='item'>How to Test It</p>
-                    <p id='contact-info' className='item'>Contact Info</p>
+                    <p id='how-it-works'
+                        className={activeAction == 'how-it-works'? 'active item': 'item'}
+                        onClick={()=>setActiveAction('how-it-works')}>
+                        How It Works
+                    </p>
+
+                    <p id='how-to-test-it'
+                        className={activeAction == 'how-to-test-it'? 'active item': 'item'}
+                        onClick={()=>setActiveAction('how-to-test-it')}>
+                        How to Test It
+                    </p>
+
+                    <p id='contact-info'
+                        className={activeAction == 'contact-info'? 'active item': 'item'}
+                        onClick={()=>setActiveAction('contact-info')}>
+                        Contact Info
+                    </p>
+
                 </div>
             </div>
         </nav>
