f49127c116b2860b33a498e66fcb06f36bf87fbc
Vincent <makokhavomondi@gmail.com>
2023-01-25 14:38:27 +0300

    Work on canvas item and button
    Change button style on hover and start working on the painting stuff on canvas

--- src/Components/Canvas Item/CanvasItem.js
+++ src/Components/Canvas Item/CanvasItem.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, {useRef} from "react";
 import "./CanvasItem.css"
 
 function CanvasItem({index}){
+    const canvasRef = useRef()
+    let painting = false
+
+    function startPainting(e, ctx){
+        painting = true
+
+    }
+
+    function finishPainting(e, ctx){
+        painting = false
+    }
+
+    function handleCanvas(e){
+        const ctx = canvasRef.current?.getContext('2d')
+
+        canvasRef.current?.addEventListener('mousedown', (e)=>startPainting(e, ctx))
+        canvasRef.current?.addEventListener('mouseup', (e)=>finishPainting(e, ctx))
+        canvasRef.current?.addEventListener('mousemove', ()=>{
+            if(painting){
+                console.log('painting')
+            }
+        })
+    }
+
+    window.addEventListener('load', handleCanvas)
+
     return (
-        <canvas id={`canvas-item-${index}`} className="canvas-item">
+        <canvas id={`canvas-item-${index}`} ref={canvasRef} className="canvas-item">
 
         </canvas>
     )
