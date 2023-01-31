14673517bfd7538597ab37fa1d2223b66ba9f85e
Vincent <makokhavomondi@gmail.com>
2023-01-31 20:29:42 +0300

    Initial styling of the canvas component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react'
 import SampleItem from './Components/Canvas Item/SampleItem';
 import SampleItemBtn from './Components/Canvas Item/SampleItemBtn';
 import Navbar from './Components/Navbar/Navbar';
+import Canvas from './Components/Canvas/Canvas'
 
 function App() {
   const [activeSample, setActiveSample] = useState(1)
@@ -10,30 +11,33 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className='container main-body'>
-        <div className='sample-items'>
-          {
-            [1, 2, 3].map(index => {
-              return (
-                <div key={`item-and-btn-${index}`} className={activeSample ===+ index? 'active item-and-btn': 'item-and-btn'}>
-                    <SampleItem  index={index}/>
-                    <SampleItemBtn index={index} activeSample={activeSample} setActiveSample={setActiveSample}/>
-                </div>
-              )
-            })
-          }
-        </div>
+      <div className='container'>
+        <Canvas />
+        <div className='sample-items-body'>
+          <div className='sample-items'>
+            {
+              [1, 2, 3].map(index => {
+                return (
+                  <div key={`item-and-btn-${index}`} className={activeSample ===+ index? 'active item-and-btn': 'item-and-btn'}>
+                      <SampleItem  index={index}/>
+                      <SampleItemBtn index={index} activeSample={activeSample} setActiveSample={setActiveSample}/>
+                  </div>
+                )
+              })
+            }
+          </div>
 
-        <div className='sample-items'>
-          {
-            [4, 5, 6].map(index => {
-              return (
-                <div key={`item-and-btn-${index}`} className={index === 4 ? 'active item-and-btn': 'item-and-btn'}>
-                  <SampleItem index={index}/>
-                </div>
-              )
-            })
-          }
+          <div className='sample-items'>
+            {
+              [4, 5, 6].map(index => {
+                return (
+                  <div key={`item-and-btn-${index}`} className={index === 4 ? 'active item-and-btn': 'item-and-btn'}>
+                    <SampleItem index={index}/>
+                  </div>
+                )
+              })
+            }
+          </div>
         </div>
       </div>
     </>
--- src/Components/Canvas/Canvas.js
+++ src/Components/Canvas/Canvas.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 import "./canvas.css"
 
 function Canvas(){
