ce0cfff0ccae1e56f095325d85271a11c5fec924
Vincent <makokhavomondi@gmail.com>
2023-02-18 19:31:01 +0300

    :sparkles: Create Pieces component

---
+++ src/Components/Pieces/Pieces.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Pieces.css"
+
+function Pieces({allPiecesCount, coloredPiecesCount, testOrSolution}){
+    let pieces = []
+    let gridAreas = [
+        'a', 'b', 'c', 'd', 'e',
+        'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o',
+        'p', 'q', 'r', 's', 't',
+        'u', 'v', 'w', 'x', 'y'
+    ]
+
+    // Shuffle the grid areas
+    gridAreas = gridAreas.sort(()=> Math.random()-0.5)
+
+
+    for(let i = 0; i < allPiecesCount; i++){
+        if(i < coloredPiecesCount){
+            pieces.push(
+                <div className={`piece-container ${gridAreas[i]}`}>
+                    <div className="colored piece"></div>
+                </div>
+            )
+        }else{
+            pieces.push(
+                <div className={`piece-container ${gridAreas[i]}`}>
+                    <div className="piece"></div>
+                </div>          
+            )
+        }
+    }
+
+    // Shuffle the pieces
+    pieces = pieces.sort(()=> Math.random()-0.5)
+
+    return (
+        <div id="pieces" className={testOrSolution}>
+            {
+                pieces
+            }
+        </div>
+    )
+}
+
+export default Pieces;
