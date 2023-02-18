988c2c868e1978cdf61777c8696961fd87e31d4d
Vincent <makokhavomondi@gmail.com>
2023-02-18 14:43:31 +0300

    :construction: Start working on the "how it works" page

--- src/Pages/HowItWorks/HowItWorks.js
+++ src/Pages/HowItWorks/HowItWorks.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./HowItWorks.css"
 
 function HowItWorks(){
     return (
-        <div>
-
+        <div id="how-it-works" className="container">
+            <p>
+                So I came up with a new zero-shot image recognition algorithm.
+                I know that sounds preposterous and the fact that it looks
+                promising makes it super interesting.
+            </p>
         </div>
     )
 }
