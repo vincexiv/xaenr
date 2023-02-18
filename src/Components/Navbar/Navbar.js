be2c48fae88f19c32eb90365965a65c622f7d5f5
Vincent <makokhavomondi@gmail.com>
2023-02-18 15:17:24 +0300

    :recycle: Make selectors for navbar styling more specific

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -13,25 +13,25 @@ function Navbar({activeAction, setActiveAction}){
                 </h1>
                 
                 <div className='actions'>
-                    <p id='how-it-works'
+                    <p id='nav-content-home'
                         className={activeAction == 'home'? 'active item': 'item'}
                         onClick={()=>setActiveAction('home')}>
                         Home
                     </p>
 
-                    <p id='how-it-works'
+                    <p id='nav-content-how-it-works'
                         className={activeAction == 'how-it-works'? 'active item': 'item'}
                         onClick={()=>setActiveAction('how-it-works')}>
                         How It Works
                     </p>
 
-                    <p id='how-to-test-it'
+                    <p id='nav-content-how-to-test-it'
                         className={activeAction == 'how-to-test-it'? 'active item': 'item'}
                         onClick={()=>setActiveAction('how-to-test-it')}>
                         How to Test It
                     </p>
 
-                    <p id='contact-info'
+                    <p id='nav-content-contact-info'
                         className={activeAction == 'contact-info'? 'active item': 'item'}
                         onClick={()=>setActiveAction('contact-info')}>
                         Contact Info
