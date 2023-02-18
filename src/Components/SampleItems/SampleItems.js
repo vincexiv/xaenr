1765233fd10dc10fd39d3d7c8c13e70e53ccd9a9
Vincent <makokhavomondi@gmail.com>
2023-02-18 23:36:53 +0300

    :recycle: Make SampleItems component return an array of sample items
    It was initially returning an element whose inside contained the array

--- src/Components/SampleItems/SampleItems.js
+++ src/Components/SampleItems/SampleItems.js
@@ -4,25 +4,26 @@ import "./SampleItems.css"
 
 function SampleItems({images, sampleType, status, setStatus, previousStatus}){
     return (
-        <div id='sample-items'>
+        <>
             {
                 images.map((image, index) => {
                     return (
                     <div
+                        id={`${sampleType}-item-and-btn-${index}`}
                         key={`item-and-btn-${index}`}
                         className={
                             (status.activeSample === index && sampleType === "sample") ?
-                            'active item-and-btn': 
+                            'active sample-items-item-and-btn': 
                             (index === 0 && sampleType === "results") ?
-                            'active item-and-btn ':
-                            'item-and-btn'
+                            'active sample-items-item-and-btn ':
+                            'sample-items-item-and-btn'
                         }>
                             {
                                 sampleType === "sample"? (
                                     <>
                                         <img
                                         id={`${sampleType}-${index}`}
-                                        className="sample-item"
+                                        className="sample-items-sample-item"
                                         src={image}
                                         alt="no image added yet. Please paint something in the canvas then click 'add'"/>
 
@@ -35,7 +36,7 @@ function SampleItems({images, sampleType, status, setStatus, previousStatus}){
                                 ): (
                                     <img
                                     id={`${sampleType}-${index}`}
-                                    className="sample-item"
+                                    className="sample-items-sample-item"
                                     src={image}
                                     alt="After matching, your results will appear here"/> 
                                 )
@@ -44,7 +45,7 @@ function SampleItems({images, sampleType, status, setStatus, previousStatus}){
                     )
                 })
             }
-      </div>
+      </>
     )
 }
 
