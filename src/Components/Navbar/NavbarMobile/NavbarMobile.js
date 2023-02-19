9fd1f5a0394db02c592828c55379aeae3c7d7790
Vincent <makokhavomondi@gmail.com>
2023-02-19 11:46:10 +0300

    :sparkles: Create NavbarMobile
    This is basically a humburger icon that would be shown when the client width reduces to a certain level to enable responsiveness

---
+++ src/Components/Navbar/NavbarMobile/NavbarMobile.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./NavbarMobile.css"
+
+function NavbarMobile(){
+    return (
+        <div id="navbar-mobile">
+            <label for="check">
+                <input type="checkbox" id="check"/> 
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+        </div>
+    )
+}
+
+export default NavbarMobile;
