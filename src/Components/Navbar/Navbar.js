6f456ca13b8a761ec8e06d54d185d69dda905add
Vincent <makokhavomondi@gmail.com>
2023-02-19 11:45:24 +0300

    :construction: Enable reusing of the company icon part for both navbar types

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,22 @@
 import React, {useState} from 'react'
 import NavbarDesktop from './NavbarDesktop/NavbarDesktop'
+import NavbarMobile from './NavbarMobile/NavbarMobile'
 import './Navbar.css'
 
 function Navbar({activeAction, setActiveAction}){
 
     return (
-        <NavbarDesktop activeAction={activeAction} setActiveAction={setActiveAction} />
+        <nav>
+            <div className='nav-content'>
+                <h1
+                    className='company-name'
+                    onClick={()=>setActiveAction('home')}>
+                    xaenr
+                </h1>
+                
+                <NavbarDesktop activeAction={activeAction} setActiveAction={setActiveAction} />
+            </div>
+        </nav>
     )
 }
 
--- src/Components/Navbar/NavbarDesktop/NavbarDesktop.js
+++ src/Components/Navbar/NavbarDesktop/NavbarDesktop.js
@@ -5,42 +5,32 @@ import "./NavbarDesktop.css"
 function NavbarDesktop({activeAction, setActiveAction}){
 
     return (
-        <nav>
-            <div className='nav-content'>
-                <h1
-                    className='company-name'
-                    onClick={()=>setActiveAction('home')}>
-                    xaenr
-                </h1>
-                
-                <div className='actions'>
-                    <p id='nav-content-home'
-                        className={activeAction == 'home'? 'active item': 'item'}
-                        onClick={()=>setActiveAction('home')}>
-                        Home
-                    </p>
+        <ul id="navbar-desktop" className='actions'>
+            <li id='nav-content-home'
+                className={activeAction == 'home'? 'active item': 'item'}
+                onClick={()=>setActiveAction('home')}>
+                Home
+            </li>
 
-                    <p id='nav-content-how-it-works'
-                        className={activeAction == 'how-it-works'? 'active item': 'item'}
-                        onClick={()=>setActiveAction('how-it-works')}>
-                        How It Works
-                    </p>
+            <li id='nav-content-how-it-works'
+                className={activeAction == 'how-it-works'? 'active item': 'item'}
+                onClick={()=>setActiveAction('how-it-works')}>
+                How It Works
+            </li>
 
-                    <p id='nav-content-how-to-test-it'
-                        className={activeAction == 'how-to-test-it'? 'active item': 'item'}
-                        onClick={()=>setActiveAction('how-to-test-it')}>
-                        How to Test It
-                    </p>
+            <li id='nav-content-how-to-test-it'
+                className={activeAction == 'how-to-test-it'? 'active item': 'item'}
+                onClick={()=>setActiveAction('how-to-test-it')}>
+                How to Test It
+            </li>
 
-                    <p id='nav-content-contact-info'
-                        className={activeAction == 'contact-info'? 'active item': 'item'}
-                        onClick={()=>setActiveAction('contact-info')}>
-                        Contact Info
-                    </p>
+            <li id='nav-content-contact-info'
+                className={activeAction == 'contact-info'? 'active item': 'item'}
+                onClick={()=>setActiveAction('contact-info')}>
+                Contact Info
+            </li>
 
-                </div>
-            </div>
-        </nav>
+        </ul>
     )
 }
 
