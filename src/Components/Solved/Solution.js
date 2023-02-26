a13d1f84dc65c11a92bc5b864da8daf089c42374
Vincent <makokhavomondi@gmail.com>
2023-02-26 12:19:23 +0300

    :sparkles: Add a component that shows when something is solved or not

---
+++ src/Components/Solved/Solution.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Solution.css"
+
+function Solution({solutionArray}){
+    let solutionPieces = []
+
+    for(let i = 0; i < solutionArray?.length; i++){
+        if(solutionArray?.[i]){
+            solutionPieces.push(
+                <div key={i} className={`piece-container`}>
+                    <div className="colored piece"></div>
+                </div>
+            )
+        }else{
+            solutionPieces.push(
+                <div key={i} className={`piece-container`}>
+                    <div className="piece"></div>
+                </div>          
+            )
+        }
+    }
+
+    console.log(solutionPieces)
+
+    return (
+        <div id="solution">
+            {
+                solutionPieces
+            }
+        </div>
+    )
+}
+
+export default Solution;
