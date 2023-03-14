b6b613ea3e31cd9241f1c5e49e8b736fcceabf75
Vincent <makokhavomondi@gmail.com>
2023-03-14 12:15:18 +0300

    Generate part 7 and 8

---
+++ src/Pages/HowItWorks/TheParts/Part7/Part7.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Part7.css"
+
+function Part7(){
+    return (
+        <div id="how-it-works--part-7">
+
+        </div>
+    )
+}
+
+export default Part7;
---
+++ src/Pages/HowItWorks/TheParts/Part8/Part8.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Part8.css"
+
+function Part8(){
+    return (
+        <div id="how-it-works--part-8">
+
+        </div>
+    )
+}
+
+export default Part8;
