1a73dcab39391cb8a3e92cf06df5eb9823d1a4bd
Vincent <makokhavomondi@gmail.com>
2023-03-14 10:47:20 +0300

    Fix some warnings

--- src/Components/Solved/Solution.js
+++ src/Components/Solved/Solution.js
@@ -5,13 +5,13 @@ function Solution({solutionArray}){
     let solutionPieces = []
 
     for(let i = 0; i < solutionArray?.length; i++){
-        if(solutionArray?.[i]  == 1){
+        if(solutionArray?.[i]  === 1){
             solutionPieces.push(
                 <div key={i} className={`piece-container`}>
                     <div className="colored piece"></div>
                 </div>
             )
-        }else if(solutionArray?.[i]  == 0){
+        }else if(solutionArray?.[i]  === 0){
             solutionPieces.push(
                 <div key={i} className={`piece-container`}>
                     <div className="piece"></div>
