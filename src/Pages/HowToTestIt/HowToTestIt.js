e7de050844cfdc21caf8a9655c6dec13b1817d50
Vincent <makokhavomondi@gmail.com>
2023-03-14 16:16:10 +0300

    Add a how it to test it video

--- src/Pages/HowToTestIt/HowToTestIt.js
+++ src/Pages/HowToTestIt/HowToTestIt.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./HowToTestIt.css"
 
 function HowToTestIt(){
-    return (
-        <div>
+    const [width, setWidth] = useState(getWidth())
 
+    useEffect(()=>{
+        window.addEventListener('resize', ()=>{
+            setWidth(getWidth())
+        })
+    }, [])
+
+    function getWidth(){
+        return 0.75*window.innerWidth
+    }
+    
+    return (
+        <div id="how-to-test-it-section" className="container">
+            <div className="wrapper">
+            <iframe src="https://drive.google.com/file/d/1g_6kL3ZdQG5PXfeGYaQhvrbrktxR6vMK/preview"
+                    width={width}
+                    height="480"
+                    allow="autoplay">
+                </iframe>
+            </div>
         </div>
     )
 }
