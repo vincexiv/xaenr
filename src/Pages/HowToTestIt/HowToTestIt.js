21ce406665b345694454b8818546bc0c479f12b2
Vincent <makokhavomondi@gmail.com>
2023-03-18 14:34:46 +0300

    Make the dimensions of how to test it video responsive

--- src/Pages/HowToTestIt/HowToTestIt.js
+++ src/Pages/HowToTestIt/HowToTestIt.js
@@ -2,24 +2,28 @@ import React, { useEffect, useState } from "react";
 import "./HowToTestIt.css"
 
 function HowToTestIt(){
-    const [width, setWidth] = useState(getWidth())
+    const [widthAndHeight, setWidthAndHeight] = useState({width: getWidth(), height: getHeight()})
 
     useEffect(()=>{
         window.addEventListener('resize', ()=>{
-            setWidth(getWidth())
+            setWidthAndHeight({width: getWidth(), height: getHeight()})
         })
     }, [])
 
     function getWidth(){
         return 0.75*window.innerWidth
     }
+
+    function getHeight(){
+        return 0.8*window.innerHeight
+    }
     
     return (
         <div id="how-to-test-it-section" className="container">
             <div className="wrapper">
             <iframe src="https://drive.google.com/file/d/1g_6kL3ZdQG5PXfeGYaQhvrbrktxR6vMK/preview"
-                    width={width}
-                    height="480"
+                    width={widthAndHeight.width}
+                    height={widthAndHeight.height}
                     allow="autoplay">
                 </iframe>
             </div>
