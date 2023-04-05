7fef73bef2cb29061949e99b86f4e16c9b77c761
Vincent <makokhavomondi@gmail.com>
2023-04-05 15:57:36 +0300

    Select files, not select some files on upload image button

--- src/Components/UploadImage/UploadImage.js
+++ src/Components/UploadImage/UploadImage.js
@@ -73,7 +73,7 @@ function UploadImage({setResults}){
             <form className="my-form">
                 <p>Upload multiple files with the file dialog or by dragging and dropping images onto the dashed region</p>
                 <input type="file" id="fileElem" multiple accept="image/*"/>
-                <label className="button" htmlFor="fileElem">Select some files</label>
+                <label className="button" htmlFor="fileElem">Select files</label>
             </form>
         </div>
       </div>
