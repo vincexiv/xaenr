fc7d76f2c57d0ca0c2f01b1417512112be71432a
Vincent <makokhavomondi@gmail.com>
2023-04-05 14:26:07 +0300

    Create a page for uploading files and viewing results

---
+++ src/Pages/GetStarted/GetStarted.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UploadImage from "../../Components/UploadImage/UploadImage";
+
+function GetStarted(){
+    return (
+        <div id="get-started">
+            <UploadImage />
+        </div>
+    )
+}
+
+export default GetStarted;
