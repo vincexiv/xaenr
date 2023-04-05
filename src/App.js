b4e31b42f897d41d2fac9a9a8de9bcc774ffb524
Vincent <makokhavomondi@gmail.com>
2023-04-05 21:05:29 +0300

    Link pages in explanation of how things work

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
           <Route exact path="/test-it" element={<TestIt setActiveAction={setActiveAction}/>} />
           <Route exact path="/get-started" element={<GetStarted setActiveAction={setActiveAction}/>} />
           <Route exact path="/contact-info" element={<ContactInfo />} />
-          <Route exact path="/sample-test-results" element={<SampleTestResults />} />
+          <Route exact path="/sample-test-results" element={<SampleTestResults setActiveAction={setActiveAction} />} />
           <Route path="/" element={<Home setActiveAction={setActiveAction} />} />
       </Routes>
     </>
--- src/Pages/SampleTestResults/SampleTestResults.js
+++ src/Pages/SampleTestResults/SampleTestResults.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import "./SampleTestResults.css"
 
 
-function SampleTestResults(){
+function SampleTestResults({setActiveAction}){
     const navigate = useNavigate()
     const images = [
         "https://drive.google.com/file/d/1f7kvsRiIjj4Yq2WAiWVGK4q9Ndk67Q7v/view?usp=sharing",
@@ -11,26 +11,24 @@ function SampleTestResults(){
         "https://drive.google.com/file/d/1f7kvsRiIjj4Yq2WAiWVGK4q9Ndk67Q7v/view?usp=sharing"
     ]
 
-    function goToTestIt(){
-        navigate('/test-it')
-    }
-
     return (
         <div id="sample-test-results" className="container">
             <h1>Sample Results</h1>
             <p>Here are sample results obtained by doodling stuff on
-                <span onClick={goToTestIt}> this page</span>
+                <span onClick={()=>setActiveAction('test-it')}> this page</span>
             </p>
             <p>
                 I encouraged doodling absolutely random stuff to prove that the 
                 algorithm wasn't trained on any data. It simply matches one image 
                 with another that has the closest match to it from a group of images. 
                 It does this by first assuming that the pieces obtained after 
-                breaking down the two images are similar. It therefore tries to 
-                map the pieces from one image to the pieces obtained in another. 
-                An image is considered the best match if after mapping, pieces 
-                that were close in the original image are mapped into a region in 
-                another image that makes those pieces close again.
+                breaking down the two images are similar. You can check the page I 
+                dedicated to explain <span onClick={()=>setActiveAction('how-it-works')}> 
+                how the algorithm works </span> to find out more. That being said, after 
+                breaking down the images, it tries to map the pieces from one image to the 
+                pieces obtained in another. An image is considered the best match if after 
+                mapping, pieces that were close in the original image are mapped into a 
+                region in another image that makes those pieces close again.
             </p>
 
             <ul id="sample-result-list">
