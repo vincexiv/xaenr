71537afa569371d35a93cd370e16d8b0cd2f4476
Vincent <makokhavomondi@gmail.com>
2023-04-23 19:58:18 +0300

    Add github link and style the iframe "How It Works" page

--- src/Pages/HowItWorks/HowItWorks.js
+++ src/Pages/HowItWorks/HowItWorks.js
@@ -5,10 +5,13 @@ import "./HowItWorks.css"
 function HowItWorks(){
     return (
         <div id="how-it-works" className="container">
+            <div id="more-info">
+                <p>Find more of the work <a href="https://github.com/VinceXIV/open-image" target={"_blank"}>on github</a></p>
+            </div>
 
             <iframe
                 src="./HowItWorks.html"
-                width={"100%"}
+                width={"80%"}
                 height={"100%"}
                 >
             </iframe>
