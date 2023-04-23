5619c8f0d8f47838ef7ff9b12c522ff0c38fcaa4
Vincent <makokhavomondi@gmail.com>
2023-04-23 20:01:39 +0300

    Fix some warnings; rel="noreferrer" and "iframe should have unique title"

--- src/Pages/HowItWorks/HowItWorks.js
+++ src/Pages/HowItWorks/HowItWorks.js
@@ -6,10 +6,15 @@ function HowItWorks(){
     return (
         <div id="how-it-works" className="container">
             <div id="more-info">
-                <p>Find more of the work <a href="https://github.com/VinceXIV/open-image" target={"_blank"}>on github</a></p>
+                <p>Find more of the work
+                    <a href="https://github.com/VinceXIV/open-image" target={"_blank"}
+                        rel="noreferrer">
+                        on github</a>
+                    </p>
             </div>
 
             <iframe
+                title="how-it-works-iframe"
                 src="./HowItWorks.html"
                 width={"80%"}
                 height={"100%"}
