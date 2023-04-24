f8326c8980ba5125ed609a56f08c327a9993baae
Vincent <makokhavomondi@gmail.com>
2023-04-24 13:16:09 +0300

    Update home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -12,13 +12,32 @@ function Home({setActiveAction}){
       <div className="home-content">
         <h3 id="hello-there">Hello there,</h3>
         <h1 id="welcome-to-xaenr">Welcome to Xaenr</h1>
-        <p>There's nothing much to show for now</p>
-        <p>visit the following links</p>
-        <p><span className="link" onClick={()=>goTo('how-it-works')}>how it works</span></p>
-        <p><span className="link" onClick={()=>goTo('intuition')}>intuition behind it</span></p>
-        <p><span className="link" onClick={()=>goTo('how-to-test-it')}>how to test it</span></p>
-        <p><span className="link" onClick={()=>goTo('sample-test-results')}>sample results</span></p>
-        <p><span className="link" onClick={()=>goTo('test-it')}>test it</span></p>
+        <p>
+          We are building a one-shot image recognition algorithm that is explainable by design.
+          So far, we've almost completed the first stage, which involved testing on random doodles and MNIST
+          digits. The next stage will involve scaling it so that it can work with bigger images.
+        </p>
+        <p>
+          The project will be available for contribution soon. To get acquainted with what we've 
+          accomplished so far, visit the links provided below
+        </p>
+        <ul>
+          <li>
+            <p><span className="link" onClick={()=>goTo('how-it-works')}>how it works</span></p>
+          </li>
+          <li>
+            <p><span className="link" onClick={()=>goTo('intuition')}>intuition behind it</span></p>
+          </li>
+          <li>
+            <p><span className="link" onClick={()=>goTo('how-to-test-it')}>how to test it</span></p>
+          </li>
+          <li>
+            <p><span className="link" onClick={()=>goTo('sample-test-results')}>sample results</span></p>
+          </li>
+          <li>
+            <p><span className="link" onClick={()=>goTo('test-it')}>test it</span></p>
+          </li>
+        </ul>
         <div id="btn-container">
           <Button classList={["get-started-btn"]} message="Get Started" onClick={()=>goTo('how-it-works')}/>
         </div>
