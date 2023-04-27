1fd5c566fdd41e1ca29a092058937272adc3d243
Vincent <makokhavomondi@gmail.com>
2023-04-27 19:27:20 +0300

    Add a link to video showing a summary of how the project works in practice

--- src/Pages/HowItWorks/HowItWorks.js
+++ src/Pages/HowItWorks/HowItWorks.js
@@ -6,8 +6,15 @@ function HowItWorks(){
     return (
         <div id="how-it-works" className="container">
             <div id="more-info">
+                <p>
+                    Checkout <a href="https://www.youtube.com/watch?v=j656VXJ3udY"
+                    className="link"
+                    target="_blank"> this video </a> 
+                    to find out a summary of how it works in practice 
+                </p>
                 <p>Find more of the work <a href="https://github.com/VinceXIV/open-image"
                     target={"_blank"}
+                    className="link"
                     rel="noreferrer">on github</a>
                 </p>
             </div>
