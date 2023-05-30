d75ff8a105ae313600bb685a5ecbb773a759fbf1
Vincent <makokhavomondi@gmail.com>
2023-05-30 13:04:13 +0300

    Show items on navbar

--- src/Components/Navbar/NavbarActionList/NavbarActionList.js
+++ src/Components/Navbar/NavbarActionList/NavbarActionList.js
@@ -9,11 +9,11 @@ function NavbarActionList({activeAction, onClick}){
                 Home
             </li>
 
-            {/* <li id='nav-content-get-started'
+            <li id='nav-content-get-started'
                 className={activeAction === 'get-started'? 'active item': 'item'}
                 onClick={()=>onClick('get-started')}>
                 Get Started
-            </li> */}
+            </li>
 
             <li id='nav-content-how-it-works'
                 className={activeAction === 'how-it-works'? 'active item': 'item'}
@@ -21,17 +21,17 @@ function NavbarActionList({activeAction, onClick}){
                 How It Works
             </li>
 
-            {/* <li id='nav-content-how-to-test-it'
+            <li id='nav-content-how-to-test-it'
                 className={activeAction === 'how-to-test-it'? 'active item': 'item'}
-                onClick={()=>setActiveAction('how-to-test-it')}>
+                onClick={()=>onClick('how-to-test-it')}>
                 How to Test It
             </li>
 
             <li id='nav-content-test-it'
                 className={activeAction === 'test-it'? 'active item': 'item'}
-                onClick={()=>setActiveAction('test-it')}>
+                onClick={()=>onClick('test-it')}>
                 Test It
-            </li> */}
+            </li>
 
             <li id='nav-content-contact-info'
                 className={activeAction === 'contact-info'? 'active item': 'item'}
